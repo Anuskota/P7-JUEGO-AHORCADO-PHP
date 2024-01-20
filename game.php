8e692812a7af0d3a9d770929231a3028133aef77
Anuskota <anamangada.dev@gmail.com>
2024-01-20 17:47:23 +0100

    functions

--- game.php
+++ game.php
@@ -9,46 +9,162 @@ $guess = "HANGMAN";
 $maxLetters = strlen($guess) - 1;
 $responses = ["H", "G", "A"];
 
+
+$bodyParts = ["cabeza", "cabeza_y_tronco", "tronco_y_mano", "tronco_y_2manos", "tronco_y_2manos_ypie", "tronco_y2_pies", "gorro", "ojos", "FIN"];
+$words = ["FEMCODERS", "SEGOVIA", "LIBERTAD", "SUEÑO", "INTELIGENCIA", "CANSALIEBRES", "GAMBITERO", "ANIS"];
+
+
+
+
+
 function getCurrentPicture($part)
 {
-    return "./img/hangman_" . $part. ".png";
-}
-$bodyParts = ["cabeza","cabeza_y_tronco","tronco_y_mano", "tronco_y_2manos", "tronco_y_2manos_ypie" ,"tronco_y2_pies","gorro", "ojos", "FIN"];
-
-function startGame(){
-    
+    return "./img/hangman_" . $part . ".png";
 }
 
-function restartGame(){
 
-    
+
+function startGame()
+{
 }
 
-function getParts() {
+function restartGame()
+{
+    session_destroy();
+    session_start();
+}
+
+function getParts()
+{
     global $bodyParts;
-    return isset($_SESSION["parts"]) ? $_SESSION["parts"] : $bodyParts;    
+    return isset($_SESSION["parts"]) ? $_SESSION["parts"] : $bodyParts;
 }
 
-function getCurrentPart(){
-        $parts = getParts();
-        return $parts[0];
-    
+function getCurrentPart()
+{
+    $parts = getParts();
+    return $parts[0];
 }
+
+// PALABRA 
+
+function getCurrentWord()
+{
+    global $words;
+    if (!isset($_SESSION["word"]) && empty($_SESSION["word"])) {
+        $key = array_rand($words);
+        $_SESSION["word"] = $words[$key];
+    }
+    return $_SESSION["word"];
+}
+
+function getcurrentResponses()
+{
+    return isset($_SESSION["responses"]) ? $_SERVER["responses"] : [];
+}
+
+function addResponse($letter)
+{
+    $responses = getCurrentResponses();
+    array_push($responses, $letter);
+    $_SESSION["responses"] = $responses;
+}
+
+function isLetterCorrect($letter)
+{
+    $word = getCurrentWord();
+    $max = strlen($word) - 1;
+    for ($i = 0; $i <= $max; $i++) {
+        if ($letter == $word[$i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isWordCorrect()
+{
+    $guess = getCurrentWord();
+    $responses = getCurrentResponses();
+    $max = strlen($guess) - 1;
+    for ($i = 0; $i <= $max; $i++) {
+        if (!in_array($guess[$i],  $responses)) {
+            return false;
+        }
+    }
+    return true;
+}
+function isBodyComplete()
+{
+    $parts = getParts();
+    // is the current parts less than or equal to one
+    if (count($parts) <= 1) {
+        return true;
+    }
+    return false;
+}
+function addPart()
+{
+    $parts = getParts();
+    array_shift($parts);
+    $_SESSION["parts"] = $parts;
+}
+// is game complete
+function gameComplete()
+{
+    return isset($_SESSION["gamecomplete"]) ? $_SESSION["gamecomplete"] : false;
+}
+
+
+// set game as complete
+function markGameAsComplete()
+{
+    $_SESSION["gamecomplete"] = true;
+}
+
+// start a new game
+function markGameAsNew()
+{
+    $_SESSION["gamecomplete"] = false;
+}
+
+
+
+
+
+
 
 //Para saber que el juegp se reinicia
 
-if(isser($_GET['start'])){
-
-    
+if (isset($_GET['start'])) {
 }
 
 //Para saber cuando se ha pulsado KeyPresset KP
-if (isser($_GET['kp'])) {
-
+if (isset($_GET['kp'])) {
     $currentPressedKey = isset($_GET['kp']) ? $_GET['kp'] : null;
+    // if the key press is correct
+    if (
+        $currentPressedKey
+        && isLetterCorrect($currentPressedKey)
+        && !isBodyComplete()
+        && !gameComplete()
+    ) {
 
-    if($currentPressedKey){
-        
+        addResponse($currentPressedKey);
+        if (isWordCorrect()) {
+            $WON = true; // game complete
+            markGameAsComplete();
+        }
+    } else {
+        // start hanging the man :)
+        if (!isBodyComplete()) {
+            addPart();
+            if (isBodyComplete()) {
+                markGameAsComplete(); // lost condition
+            }
+        } else {
+            markGameAsComplete(); // lost condition
+        }
     }
 }
 
@@ -73,7 +189,7 @@ if (isser($_GET['kp'])) {
     <div style=" margin :0 auto; background: #dddddd; width:900px; height:900px; padding:5px; border-radius:3px ">
 
         <div style="width:500px; display:inline-block; background:#fff1;">
-            <img src="<?php echo getCurrentPicture(getCurrentPart());?> " alt=""
+            <img src="<?php echo getCurrentPicture(getCurrentPart()); ?> " alt=""
                 style=" width :80%; display:inline-block;">
         </div>
 
@@ -95,7 +211,9 @@ if (isser($_GET['kp'])) {
 
 
                     ?>
+                    <br><br>
 
+                    <button type="submit" name="start">RESTART GAME</button>
                 </form>
             </div>
         </div>
