e5128f9acba991cf0ee828417af2040378ac3a68
Anuskota <anamangada.dev@gmail.com>
2024-01-20 15:56:07 +0100

    fist commit

---
+++ game.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego del Ahorcado</title>
+</head>
+
+<body style="background: deepskyblue;">
+    <div style=" margin:0 auto; background: #dddddd; width:900px; padding:5px; border-radius:3px ">/div>
+
+
+
+</body>
+
+</html>
